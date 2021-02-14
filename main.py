27b0f7cfc20b69938385afe5f6b2d822f615bbcf
tozuky <77688149+tozuky@users.noreply.github.com>
2021-02-14 20:30:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from graphdisplayer import DrawGraph
-from dependencies.shortpath import astar
-from converterV2 import *
 from pathlib import Path
-from random import choice
+from converterV2 import *
+from graph_algorithms import *
+from display import *
+from debug import *
 
-OG = {
+graphTest_1 = {
         ( 0, 0) : [( 0,-1),( 0, 1),( 2, 0),( 0,-2)],
         ( 0, 1) : [( 0,-2),( 2, 1),( 0, 0)],
         ( 0,-2) : [( 0, 1),( 2,-2),(-1,-2),( 0, 0)],
@@ -26,10 +26,26 @@ OG = {
             #      :
             #      y
 
-G = getGraphFrom(Path(__file__) / "../ressources/massy") # TODO: optimiser le code de converter (ça prend trop de temps)
+showMessage = True
 
-#start = (48.7190002, 2.2533135)
-#finish = (48.7189578, 2.2533583)
-#path, cost = astar(G, start, finish)
+"""Get graph from file if filepath isn't equals to None, else get a test graph"""
+def getGraph(file = None) :
+    if file :
+        return getGraphFromFile(Path(__file__) / file, showMessage)
+    return graphTest_1
 
-#DrawGraph(G, path)
+"""Get path from graph with the specified function. Allow user to select best way or just a classic way"""
+def getPathFromGraph(graph, start, end, shortest = True) :
+    if shortest:
+        return findShortestPath(graph, start, end)
+    return findPath(graph, start, end)
+
+"""Main, code executed in first"""
+def main(start, end, file = None):
+    graph = getGraph(file) # Get a graph
+    #path = getPathFromGraph(graph, start, end) # Get a path
+    #displayGraph(graph, path) # Display the graph to the user's screen
+
+"""main(starting coordinates, ending coordinates, "File to load")"""
+#main((48.7189581, 2.2533580), (48.7189578, 2.2533583), "../ressources/massy")
+debugConverterPerformanceComparator(Path(__file__) / "../ressources/massy", 15, showMessage = True)
